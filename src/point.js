6796128f4eb78ff220ab47634e2c0cacb3393ed0
leon <leon@Bens-Mac-mini.local>
2020-01-19 17:01:45 +0200

    npm i --save-dev typescript@3.4.5 pgrep "ng serve" kill -9 <process_id>

--- src/point.js
+++ src/point.js
@@ -1,12 +1,34 @@
 var Point = /** @class */ (function () {
     function Point() {
     }
-    Point.prototype.getDistance = function () {
-        console.log("X : " + this.x + " , Y : " + this.y + " , distance: " + (this.x - this.y));
+    Point.prototype.getDraw = function () {
+        console.log("X : " + this.x + " , Y : " + this._y + " , sum x-y : " + (this.x - this._y));
     };
+    Point.prototype.getDistance = function (anotherPoint) {
+        var newX = anotherPoint.x - this.x;
+        var newY = anotherPoint.y - this.y;
+        console.log("Distance x: " + newX + " , y: " + newY);
+    };
+    Object.defineProperty(Point.prototype, "x", {
+        get: function () { return this._x; },
+        set: function (x) {
+            if (x && x.length > 2) {
+                throw new Error("x has a max length of  " + x.length);
+            }
+            this._x = x;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Point.prototype, "y", {
+        get: function () { return this._y; },
+        set: function (y) { this._y = y; },
+        enumerable: true,
+        configurable: true
+    });
     return Point;
 }());
 var point = new Point();
-point.x = 10;
+point.x = 100;
 point.y = 5;
-point.getDistance();
+point.getDraw();
--- src/point.ts
+++ src/point.ts
@@ -1,14 +1,29 @@
 class Point {
-    x: number;
-    y: number;
+    private _x: number;
+    private _y: number;
 
-    getDistance() {
-        console.log(`X : ${this.x} , Y : ${this.y} , distance: ${this.x - this.y}`);
+    getDraw() {
+        console.log(`X : ${this.x} , Y : ${this._y} , sum x-y : ${this.x - this._y}`);
 
     }
+    getDistance(anotherPoint: Point) {
+        let newX = anotherPoint.x - this.x;
+        let newY = anotherPoint.y - this.y;
+        console.log(`Distance x: ${newX} , y: ${newY}`);
+    }
+    set x(x: any) {
+        if (x && x.length > 2) {
+            throw new Error(`x has a max length of  ${x.length}`);
+        }
+        this._x = x;
+    }
+    set y(y: any) { this._y = y; }
+    get x() { return this._x; }
+    get y() { return this._y; }
+
 }
 
 let point = new Point();
-point.x = 10;
+point.x = 100;
 point.y = 5;
-point.getDistance();
+point.getDraw();
